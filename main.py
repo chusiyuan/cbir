b0931da5890055501af41903362694b31bc4ce30
chusiyuan <chusiyuan96@gmail.com>
2018-10-27 09:22:27 +0800

    add

--- main.py
+++ main.py
@@ -1,39 +1,74 @@
-import processor
 import searchor
 import numpy as np
 import matplotlib.pyplot as plt
+import pickle
+import get_features
 
-if __name__ == "__main__":
-    processor = processor.Processor('.\\dataset\\image')
-    # processor.preprocess()
-    # processor.get_features_sift()
-    # gray = processor.load_im('./dataset/image/A0C573/A0C573_20151029151740_6565273745.jpg')
-    # kps, fs = processor.get_feature_sift(gray)
-    # path = processor.get_im_path()
-    # print(path)
-    # features_after_bow = processor.bow()
-    # print(features_after_bow)
-    # np.save('features_after_bow.npy', features_after_bow)
-    # features_after_bow = np.load('features_after_bow.npy')
-    # features_tf_idf = processor.tf_idf(features_after_bow)
-    # print(features_tf_idf)
-    # np.save('features_tf_idf', features_tf_idf)
 
-    processor.get_im_path()
-    features = np.load('features_tf_idf.npy')
-
-    searchor = searchor.Searchor(features, processor.path_list)
+def pr_draw(im_path):
+    features = np.load('features_tf_idf_1.npy')
+    with open('path_list.pk', 'rb') as f:
+        path_list = pickle.load(f)
+    centers = np.load('features_centers_1.npy')
+    idf = np.load('features_idf_1.npy')
+    s = searchor.Searchor(features, path_list, centers, idf)
     precision_list = []
     recall_list = []
-    for n in range(1, 31):
-        precision, recall = searchor.search('.\\dataset\\image\\A1F201\\A1F201_20151030112626_6570241117.jpg', n)
+    query, type_num, type = s.do_query(im_path)
+    for n in range(1, 31, 1):
+        # print(n)
+        precision, recall, result, result_index = s.search(query, n, type_num, type)
+        # precision, recall, result = s.search('.\\dataset\\image\\A1F201\\A1F201_20151030112626_6570241117.jpg', n)
         # precision, recall = searchor.search('.\\dataset\\image\\A0C573\\A0C573_20151029151740_6565273745.jpg', n)
         precision_list.append(precision)
         recall_list.append(recall)
-    print(precision_list)
-    print(recall_list)
+    # print(precision_list)
+    # print(recall_list)
 
     plt.plot(recall_list, precision_list)
     plt.xlabel('recall')
     plt.ylabel('precision')
+    plt.xlim((0, 1.1))
+    plt.ylim((0, 1.1))
     plt.show()
+
+
+def test(im_path):
+    features = np.load('features_tf_idf_2.npy')
+    with open('path_list_2.pk', 'rb') as f:
+        path_list = pickle.load(f)
+    centers = np.load('features_centers_2.npy')
+    idf = np.load('features_idf_2.npy')
+    s = searchor.Searchor(features, path_list, centers, idf)
+    # precision, recall, result, result_index = s.search('.\\dataset\\image\\A1F201\\A1F201_20151030112626_6570241117.jpg', 30)
+    # '.\\dataset\\image\\A1NS85\\A1NS85_20151031103101_3022940881.jpg'
+    query, type_num, type = s.do_query(im_path)
+    precision, recall, result, result_index = s.search(query, 30, type_num, type)
+    g = get_features.GetFeatures(result, 15)
+    print(result)
+    new_result = s.resort(g.features_tf_idf, g.centers, im_path, g.idf, result)
+    print(new_result)
+
+    precision_list_1 = []
+    precision_list_2 = []
+    for n in range(1, 26, 1):
+        precision, recall, result, result_index = s.search(query, n, type_num, type)
+        precision_list_1.append(precision)
+        new_query = np.zeros(shape=features[0].shape)
+        for i in range(len(result_index)):
+            new_query += features[result_index[i]]
+        new_query /= len(result_index)
+        precision2, recall2, result2, result_index2 = s.search2(im_path, new_query, n)
+        precision_list_2.append(precision2)
+
+    plt.plot(range(1, 26, 1), precision_list_1)
+    plt.plot(range(1, 26, 1), precision_list_2)
+    plt.show()
+
+
+if __name__ == "__main__":
+    # g = get_features.GetFeatures('.\\dataset\\image', 100)
+    # pr_draw('.\\dataset\\image\\A1F201\\A1F201_20151030112626_6570241117.jpg')
+    test('.\\dataset\\image\\A3XE95\\A3XE95_20151203073732_6787841316.jpg')
+    # test('.\\dataset\\image\\A1C133\\A1C133_20151129074004_6762833645.jpg')
+
--- searchor.py
+++ searchor.py
@@ -2,13 +2,16 @@ import numpy as np
 import math
 import heapq
 import os
+import cv2
 
 
 class Searchor:
-    def __init__(self, features, path_list):
+    def __init__(self, features, path_list, centers, idf):
         self.features = features
         self.path_list = path_list
         self.file_type = ['jpg', 'bmp', 'png']
+        self.centers = centers
+        self.idf = idf
 
     def euclidean(self, mat1, mat2):
         mat1 = mat1.reshape(1, -1)
@@ -23,7 +26,101 @@ class Searchor:
         cos = (np.dot(mat1, mat2.T)[0][0])/(math.sqrt(np.dot(mat1, mat1.T)[0][0])*math.sqrt(np.dot(mat2, mat2.T)[0][0]))
         return cos
 
-    def search(self, im_path, n):
+    def do_query(self, im_path):
+        type_num = 0
+        type = im_path.split('\\')[-2]
+        im_dir = '\\'.join(im_path.split('\\')[:-1])
+        # print(im_dir)
+        # print(type)
+        for dirpath, dirnames, filenames in os.walk(im_dir):
+            for filename in filenames:
+                if filename.split('.')[-1] in self.file_type:
+                    type_num += 1
+                    # print(type_num)
+        query = np.zeros(shape=self.features[0].shape)
+        im = cv2.imread(im_path)
+        im_gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+        sift = cv2.xfeatures2d.SIFT_create()
+        kps, features = sift.detectAndCompute(im_gray, None)
+        for i in range(features.shape[0]):
+            max_simi = 0
+            max_index = 0
+            for j in range(self.centers.shape[0]):
+                simi = self.cos_simi(features[i], self.centers[j])
+                if simi > max_simi:
+                    max_simi = simi
+                    max_index = j
+            query[max_index] += 1
+
+        tf = np.zeros(shape=query.shape)
+        s = sum(query)
+        for i in range(query.shape[0]):
+            tf[i] = query[i] / s
+
+        query_tf_idf = tf * self.idf
+        return query_tf_idf, type_num, type
+
+    def search(self, query_tf_idf, n, type_num, type):
+        num = 0
+        simi_list = []
+        for i in range(len(self.features)):
+            simi = self.cos_simi(query_tf_idf, self.features[i])
+            simi_list.append(simi)
+        result_index = list(map(simi_list.index, heapq.nlargest(n, simi_list)))
+        # print('n = ' + str(n))
+        result = []
+        for i in range(len(result_index)):
+            name_i = self.path_list[result_index[i]].split('\\')[-2]
+            if name_i == type:
+                num += 1
+            # print(self.path_list[result_index[i]])
+            result.append(self.path_list[result_index[i]])
+        # print('------------------------------------------------------------')
+        precision = num / n
+        recall = num / type_num
+        return precision, recall, result, result_index
+
+    def resort(self, features_tf_idf, centers, im_path, idf, result):
+        query = np.zeros(shape=features_tf_idf[0].shape)
+        im = cv2.imread(im_path)
+        im_gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+        sift = cv2.xfeatures2d.SIFT_create()
+        kps, features = sift.detectAndCompute(im_gray, None)
+        for i in range(features.shape[0]):
+            max_simi = 0
+            max_index = 0
+            for j in range(centers.shape[0]):
+                simi = self.cos_simi(features[i], centers[j])
+                if simi > max_simi:
+                    max_simi = simi
+                    max_index = j
+            query[max_index] += 1
+
+        tf = np.zeros(shape=query.shape)
+        s = sum(query)
+        for i in range(query.shape[0]):
+            tf[i] = query[i] / s
+
+        query_tf_idf = tf * idf
+        query_list = query_tf_idf.tolist()
+        query_type = query_list.index(max(query_list))
+        # print('query type:')
+        # print(query_type)
+        new_result = []
+        temp_list = []
+        for i in range(features_tf_idf.shape[0]):
+            temp = features_tf_idf[i].tolist()
+            temp_type = temp.index(max(temp))
+            # print('temp type:')
+            # print(temp_type)
+            if query_type == temp_type:
+                new_result.append(result[i])
+            else:
+                temp_list.append(result[i])
+        new_result += temp_list
+        return new_result
+
+    def search2(self, im_path, new_query, n):
         num = 0
         type_num = 0
         simi_list = []
@@ -36,28 +133,31 @@ class Searchor:
                 if filename.split('.')[-1] in self.file_type:
                     type_num += 1
         # print(type_num)
-        query_index = self.path_list.index(im_path)
-        query = self.features[query_index]
+        query_tf_idf = new_query
+
         for i in range(len(self.features)):
-            if i == query_index:
-                simi_list.append(1)
-            else:
-                simi = self.cos_simi(query, self.features[i])
-                simi_list.append(simi)
-        # print(simi_list)
+            simi = self.cos_simi(query_tf_idf, self.features[i])
+            simi_list.append(simi)
         result_index = list(map(simi_list.index, heapq.nlargest(n, simi_list)))
-        # print(result_index)
-        print('n = ' + str(n))
-        # for i in result_index[1:]:
+        # print('n = ' + str(n))
+        result = []
         for i in range(len(result_index)):
             name_i = self.path_list[result_index[i]].split('\\')[-2]
             if name_i == type:
                 num += 1
-            print(self.path_list[result_index[i]])
-        print('------------------------------------------------------------')
+            # print(self.path_list[result_index[i]])
+            result.append(self.path_list[result_index[i]])
+        # print('------------------------------------------------------------')
         precision = num / n
         recall = num / type_num
-        return precision, recall
+        return precision, recall, result, result_index
+
+
+
+
+
+
+
 
 
 
